166ac271e248123e2f3ef2d78afbc1d0c24532be
Gabs <gabrielsilvaf@protonmail.com>
2020-08-03 17:42:53 -0300

    🎉 0.0.3 - Outras mudanças. 🎉

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import logo from './logo.png';
 function App() {
   return (
     <div id="inicio">
-      <nav class="uk-navbar-container" uk-navbar>
+      <nav class="uk-navbar-container-transparent" uk-navbar>
         <div class="uk-navbar-left">
           <ul class="uk-navbar-nav">
-            <li class="uk-parent"><a href="/">Home</a></li>
-            <li class="uk-parent"><a href="sobre">Sobre</a></li>
+            <li class="uk-active"><a href="/">Home</a></li>
+            <li class="uk-active"><a href="sobre">Sobre</a></li>
           </ul>
         </div>
       </nav>
@@ -20,8 +20,10 @@ function App() {
             <img src={logo} alt="teste" width="100" />
           </div>
         </div>
-        <div class="ola">
-        <h2>Bem vindo (a) ao Scribble</h2>
+        <div class="cadastrologin">
+         <h3>Crie sua conta gratuitamente <br />e comece a compartilhar ideias!</h3>
+         <a href="cadastro"><button class="btn">Cadastrar-se</button></a>
+         <a href="cadastro"><button class="btnlogin ">Login</button></a>
       </div>
       </div>
   );
