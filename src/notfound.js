2677c88c7a95942de768526b74e6e484b251350e
Gabs <gabrielsilvaf@protonmail.com>
2020-08-11 00:56:09 -0300

    0.0.0 - again

--- src/notfound.js
+++ src/notfound.js
@@ -4,8 +4,10 @@ import './assets/css/notfound.css';
 import Logo from "./logo.png";
 
 function App() {
+  document.title = 'Página não encontrada..'
   return (
     <div className="App">
+          <title>Página não encontrada</title>
       <header className="notfound">
         <img src={Logo} alt="logo" className="spin_ani" width="50" /> 
         <p>Aqui não tem nada, volte para a <a href="/" className="home">homepage</a>.</p>
