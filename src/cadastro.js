c3b4303eb5ef3ce10ad7fda1bb53fedc1fd5f07c
Gabs <gabrielsilvaf@protonmail.com>
2020-10-15 20:20:58 -0300

    0.0.3 - mudanças

--- src/cadastro.js
+++ src/cadastro.js
@@ -3,7 +3,6 @@ import './assets/css/main.css';
 import './assets/css/btn.css';
 import './assets/css/cadastro.css';
 import logo from './logo.png';
-import { useField } from '@unform/core';
 
 function Cadastro() {
   return (
@@ -22,6 +21,12 @@ function Cadastro() {
       </ul>
       <div id="cadastro"> 
       <h1>Crie sua conta.</h1>
+      <form id="cadastro">
+        <input type="text" placeholder="Nome" minlenght="5"></input>
+        <input type="text" placeholder="Usuário" minLength="6"></input>
+        <input type="password" placeholder="Senha" minLength="8"></input>
+        <input type="submit" id="enviar"></input>
+      </form>
       </div>    
       </div>
     );
